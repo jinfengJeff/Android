84b8cc2632274cea9ed968b1b96821f72c7a94ba
zwzn1491 <qianjf@jwele.com.cn>
2023-02-20 14:21:29 +0800

    获取实时时间（以本机时间为准）

--- TimeHelper/app/src/main/java/com/example/timehelper/MainActivity.java
+++ TimeHelper/app/src/main/java/com/example/timehelper/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 
 import com.example.timehelper.util.FetchTimeThread;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     public static Handler mHandler;
@@ -19,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(() -> {
+
+            while (true) {
+                long currentTime = System.currentTimeMillis();
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+                Date date = new Date(currentTime);
+
+                String timestampStr = formatter.format(date);
+
+                Message message = new Message();
+                message.obj = timestampStr;
+                mHandler.sendMessage(message);
+
+            }
+
+        }).start();
+
 
         mHandler = new Handler(Looper.getMainLooper()) {
             @Override
@@ -29,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        new FetchTimeThread().start(); // 获取网络时间（基于淘宝API）
+//        new FetchTimeThread().start(); // 获取网络时间（基于淘宝API）
 
     }
 }
